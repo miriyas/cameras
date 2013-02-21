c270ae91c8f00b775b005ae1a74ee162d15663db
miriya <miriya.lee@gmail.com>
2013-02-21 20:10:13 +0900

    index

---
+++ web/app/controllers/admin/cameras_controller.rb
@@ -0,0 +1,48 @@
+class Admin::CamerasController < ApplicationController
+
+  def index
+    @cameras = Camera.all
+  end
+
+  def show
+    @camera = Camera.find(params[:id])
+  end
+
+  def create
+    @camera = Camera.new(params[:camera])
+
+    respond_to do |format|
+      if @camera.save
+        format.html { redirect_to @camera, notice: 'Camera was successfully created.' }
+        format.json { render action: "show", handlers: :jbuilder }
+      else
+        format.html { render action: "new" }
+        format.json { render json: '', status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @camera = Camera.find(params[:id])
+    
+    respond_to do |format|
+      if @camera.update_attributes(params[:post])
+        format.html { redirect_to @camera, notice: 'Camera was successfully updated.' }
+        format.json { render action: "show", handlers: :jbuilder }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: '', status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @camera = Camera.find(params[:id])
+    @camera.destroy
+
+    respond_to do |format|
+      format.html { redirect_to cameras_url }
+      format.json { head :no_content }
+    end
+  end
+end
--- web/config/initializers/secret_token.rb
+++ web/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Web::Application.config.secret_token = 'b6b7a02a8131e52f03a5c998ebb49519486a0bc1dfb1787c8692cd3c47e551aa146469484bbb2e3df0cf27ff2de0bb683962e125f731286f61836df76f192e78'
+Cameras::Application.config.secret_token = 'b6b7a02a8131e52f03a5c998ebb49519486a0bc1dfb1787c8692cd3c47e551aa146469484bbb2e3df0cf27ff2de0bb683962e125f731286f61836df76f192e78'
